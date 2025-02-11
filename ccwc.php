412e00c62174f554150c902797eb9a7ddd58a3ae
Matt Gorle <3032672+mattgorle@users.noreply.github.com>
2025-02-11 23:50:01 +0000

    Implement line count (step 2)

--- ccwc.php
+++ ccwc.php
@@ -1,14 +1,27 @@
 #!/usr/bin/env php
 <?php
 
-$mode = $argv[1];
-$filename = $argv[2];
-$contents = file_get_contents($filename);
+const MODE_CHARACTER = 'c';
+const MODE_LINE = 'l';
+const MODE_WORD = 'w';
+
+$filenamePos = null;
+$modes = getopt('cwl', [], $filenamePos);
+$filename = array_slice($argv, $filenamePos);
+
+$contents = file_get_contents($filename[0]);
 
 $output = [];
 
-$output[] = strlen($contents);
-$output[] = $filename;
+if (array_key_exists(MODE_LINE, $modes)) {
+	$output[] = count(array_filter(explode(PHP_EOL, $contents), 'strlen'));
+}
+
+if (array_key_exists(MODE_CHARACTER, $modes)) {
+	$output[] = strlen($contents);
+}
+
+$output[] = $filename[0];
 
 echo implode(' ', $output);
 echo PHP_EOL;
