98f1b3321538bbd5a6d11a88e416c5d1f31cbec3
Matt Gorle <3032672+mattgorle@users.noreply.github.com>
2025-02-12 10:31:00 +0000

    Add multibyte character count support

--- ccwc.php
+++ ccwc.php
@@ -1,12 +1,13 @@
 #!/usr/bin/env php
 <?php
 
+const MODE_MB_CHARACTER = 'm';
 const MODE_CHARACTER = 'c';
 const MODE_LINE = 'l';
 const MODE_WORD = 'w';
 
 $filenamePos = null;
-$modes = getopt('cwl', [], $filenamePos);
+$modes = getopt('clmw', [], $filenamePos);
 $filename = array_slice($argv, $filenamePos);
 
 $contents = file_get_contents($filename[0]);
@@ -19,6 +20,10 @@ if (array_key_exists(MODE_LINE, $modes)) {
 
 if (array_key_exists(MODE_CHARACTER, $modes)) {
 	$output[] = strlen($contents);
+}
+
+if (array_key_exists(MODE_MB_CHARACTER, $modes)) {
+	$output[] = mb_strlen($contents);
 }
 
 $output[] = $filename[0];
